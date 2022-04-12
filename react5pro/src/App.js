31bf994eaba0d535d6471076eba0a57498ddca1a
gulshan2108 <gulshan.s2108@gmail.com>
2022-04-12 10:25:15 +0530

    modify some file

--- react5pro/src/App.js
+++ react5pro/src/App.js
@@ -1,25 +1,29 @@
+
 import React from "react";
 import Home from "./Component/Home";
-import LoginForm from "./Component/LoginForm";
-import Registration from "./Component/Registration";
+import Table from "./Component/Table";
+import From from "./Component/From";
+import { ToastContainer} from 'react-toastify';
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 function App() {
   return (
     <div className="App">
+      <ToastContainer/>
       <Router>
         <div className="header">
           <Link to="/">Home</Link>
-          <Link to="/userlogin">LoginForm</Link>
-          <Link to="/user-registration">Registration</Link>
+          <Link to="/userfrom">Form</Link>
+          <Link to="/userlist">Table</Link>
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/userlogin" element={<LoginForm />} />
-          <Route path="/user-registration" element={<Registration/>} />
+          <Route path="/userfrom" element={<From />} />
+          <Route path="/userlist" element={<Table />} />
+          <Route path="/userfrom/:id" element={<From />} />
         </Routes>
       </Router>
     </div>
   );
-}
-export default App;
+  }
+  export default App
