3f27c73c2bbead14797711da160ed5b566741b1e
sonali <sonaliyuvasoft363@gmail.com>
2022-04-20 16:32:51 +0530

    change5

--- react5pro/src/App.js
+++ react5pro/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import Home from "./Component/Home";
 import Table from "./Component/Table";
 import From from "./Component/From";
+import About from "./Component/About";
 import { ToastContainer} from 'react-toastify';
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
@@ -15,12 +16,15 @@ function App() {
           <Link to="/">Home</Link>
           <Link to="/userfrom">Form</Link>
           <Link to="/userlist">Table</Link>
+          <Link to="/about">About</Link>
+
         </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/userfrom" element={<From />} />
           <Route path="/userlist" element={<Table />} />
           <Route path="/userfrom/:id" element={<From />} />
+          <Route path="about" element={<About/>}/>
         </Routes>
       </Router>
     </div>
